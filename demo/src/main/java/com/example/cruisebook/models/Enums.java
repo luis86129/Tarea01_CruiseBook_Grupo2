dd9a886d4289c9384db16895d65ddd30d314d781
Luis Mendoza <luismendoza86129@gmail.com>
2026-06-27 23:12:29 -0500

    enums Creados

---
+++ demo/src/main/java/com/example/cruisebook/models/Enums.java
@@ -0,0 +1,9 @@
+package com.example.cruisebook.models;
+
+public class Enums {
+    public enum TipoCabina { SUITE, BALCON, INTERIOR, FAMILIAR }
+    public enum EstadoCabina { DISPONIBLE, RESERVADA, OCUPADA, MANTENIMIENTO }
+    public enum CanalNotificacion { EMAIL, SMS, APP_MENSAJERIA }
+    public enum CategoriaIncidente { RESERVA, PAGO, SERVICIO_A_BORDO }
+    public enum EstadoTicket { ABIERTO, ESCALADO, RESUELTO }
+}
